65c84a4a4f0eb5125fa730ac2094b8f1f171ac4f
angelicapages <angelicapages@gmail.com>
2020-09-04 21:38:03 -0300

    Ahora funcniona el botón limpiar

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 const limpiando = document.querySelector("#botonLimpiado")
 const buscar = document.querySelector("#buscador")
-const checkbox = document.getElementsByClassName("checkboxes")
+const checkbox = document.querySelectorAll(".checkboxes")
 
 
 
 const filtrarlimpado = () => {
     buscar.value = " "
-}
+
 
 for (let check of checkbox) {
     console.log (check.checked)
     check.checked = false
+    }
+
 }
 
 limpiando.onclick = () => {
     filtrarlimpado()
 }
+
 
 console.log (checkbox)
