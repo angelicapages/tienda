09b2336c71db0e684ac8223e65140410ed68a8aa
angelicapages <angelicapages@gmail.com>
2020-10-11 15:32:31 -0300

    ya aparece y desaparece el contador

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+//QUERYSELECTORS
 const limpiando = document.querySelector("#botonLimpiado")
 const buscar = document.querySelector("#buscador")
 const checkbox = document.querySelectorAll(".checkboxes")
@@ -5,14 +7,14 @@ const cerrarMenuHamburguesa = document.querySelector(".cierre-menu-hamburguesa")
 const abrirMenuHamburguesa = document.querySelector(".abrir-menu-hamburguesa")
 const overlay = document.querySelector("#overlay")
 const menuHamburguesa = document.querySelector("#menu-hamburguesa")
-const hidden = document.querySelector (".hidden")
-
-
+const hidden = document.querySelector(".hidden")
+const comprar = document.querySelector(".comprar-carrito")
+const checkout = document.querySelector("#menu-hamburguesa2")
+const overlay2 =document.querySelector("#overlay2")
 //BOTON LIMPIADO
+
 const filtrarlimpiado = () => {
     buscar.value = " "
-
-
     for (let check of checkbox) {
         console.log(check.checked)
         check.checked = false
@@ -27,12 +29,19 @@ limpiando.onclick = () => {
 // CERRAR Y ABRIR EL MENU HAMBURGUESA
 
 abrirMenuHamburguesa.onclick = () => {
-    menuHamburguesa.classList.remove ("hidden");
-    overlay.classList.remove ("hidden")
+    menuHamburguesa.classList.remove("hidden")
+    overlay.classList.remove("hidden")
 }
 
 cerrarMenuHamburguesa.onclick = () => {
-    menuHamburguesa.classList.add ("hidden")
-    overlay.classList.add ("hidden")
+    menuHamburguesa.classList.add("hidden")
+    overlay.classList.add("hidden")
+}
+
+// ABRIR Y CERRAR EL MENU DE CONTADOR DE CARRITO
+
+comprar.onclick = () => {
+    checkout.classList.remove("hidden")
+    overlay2.classList.remove("hidden")
 }
 
