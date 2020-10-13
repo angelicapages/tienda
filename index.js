396ad7e61c16a7747a452d704bd3e34cc2cd3c2f
angelicapages <angelicapages@gmail.com>
2020-10-13 11:01:01 -0300

    Ahora se puede hacer el calculo de total

--- index.js
+++ index.js
@@ -10,10 +10,20 @@ const menuHamburguesa = document.querySelector("#menu-hamburguesa")
 const hidden = document.querySelector(".hidden")
 const comprar = document.querySelector(".comprar-carrito")
 const checkout = document.querySelector("#menu-hamburguesa2")
-const overlay2 =document.querySelector("#overlay2")
-const seguirComprando =document.querySelector(".seguir-comprando")
+const overlay2 = document.querySelector("#overlay2")
+const seguirComprando = document.querySelector(".seguir-comprando")
+const subtotal = document.querySelector("#subtotal")
+const recargo = document.querySelector("#recargo")
+const descuento = document.querySelector("#descuento")
+const envio = document.querySelector("#envio")
+const total = document.querySelector("#total")
+const formasDePago = document.querySelectorAll(".formasDePago")
+const efectivo = document.querySelector("#efectivo")
+const credito = document.querySelector("#credito")
+const envioSiNo = document.querySelector("#envioChecked")
+const tarjetaDescuento = document.querySelector("#descuentoChecked")
 
-
+console.log
 //BOTON LIMPIADO
 
 const filtrarlimpiado = () => {
@@ -51,4 +61,68 @@ comprar.onclick = () => {
 seguirComprando.onclick = () => {
     checkout.classList.add("hidden")
     overlay2.classList.add("hidden")
+}
+
+//CALCULOS DE PRECIO EN CARRITO
+let subtotalCompra = Number(subtotal.dataset.precio)
+
+console.log(subtotalCompra)
+
+for (let opcion of formasDePago) {
+    opcion.oninput = () => {
+        calculototal()
+    }
+}
+
+let precioRecargo
+
+const cargoTarjeta = () => {
+    precioRecargo = subtotalCompra * 0.1
+    recargo.textContent = precioRecargo
+    return precioRecargo
+}
+
+let precioEnvio
+
+const cargoEnvio = () => {
+    precioEnvio = 50
+    envio.textContent = precioEnvio
+    return precioEnvio
+}
+
+let precioDescuento
+
+const agregoDescuento = () => {
+    precioDescuento = -subtotalCompra * 0.1
+    descuento.textContent = precioDescuento
+    return precioDescuento
+}
+
+const calculototal = () => {
+    if (credito.checked) {
+        precioRecargo = cargoTarjeta()
+    } else {
+        precioRecargo = 0
+        recargo.textContent = precioRecargo
+    }
+
+    if (envioSiNo.checked) {
+        precioEnvio = cargoEnvio()
+
+    } else {
+        precioEnvio = 0
+        envio.textContent = precioEnvio
+
+    }
+
+    if (tarjetaDescuento.checked) {
+        precioDescuento = agregoDescuento()
+    } else {
+        precioDescuento = 0
+        descuento.textContent = precioDescuento
+    }
+
+    let totalTodo = subtotalCompra + precioEnvio + precioRecargo + precioDescuento
+    total.textContent = totalTodo
+    return totalTodo
 }
